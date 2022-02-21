4fe69c25c1faca58563d13d531b2a4ae5782ea10
NguyenQuangHuu <nguyenquanghuu2010@gmail.com>
2022-02-21 21:23:21 +0700

    sua loi

--- src/ss6_ke_thua/bai_tap/point_2d_3d/Point3D.java
+++ src/ss6_ke_thua/bai_tap/point_2d_3d/Point3D.java
@@ -2,7 +2,37 @@ package ss6_ke_thua.bai_tap.point_2d_3d;
 
 public class Point3D extends  Point2D{
     private float z;
-    public Point3D(){
-        
+
+    public Point3D() {
+    }
+
+    public Point3D(float x, float y, float z){
+        super(x,y);
+        this.z = z;
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+    }
+
+    public void setXYZ(float x, float y,float z){
+        this.setX(x);
+        this.setY(y);
+        this.z = z;
+    }
+
+    public float[] getXYZ(){
+        return new float[]{this.getX(),this.getY(),this.getZ()};
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "Point3D{" +
+                "z=" + z +
+                '}';
     }
 }
