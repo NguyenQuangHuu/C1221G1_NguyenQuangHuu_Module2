a66bf479ddb8b59c0fe22f785e8b8be2924d680e
NguyenQuangHuu <nguyenquanghuu2010@gmail.com>
2022-02-21 21:21:35 +0700

    sua loi

--- src/ss1_introduction_java/bai_tap/ShowHello.java
+++ src/ss1_introduction_java/bai_tap/ShowHello.java
@@ -4,25 +4,9 @@ import java.util.Scanner;
 
 public class ShowHello {
     public static void main(String[] args) {
-        System.out.println("Linear Equal Resolver");
-        System.out.println("Given a equation as 'a * x + b = c', please enter constants: ");
         Scanner sc = new Scanner(System.in);
-        System.out.println("a :");
-        int a = sc.nextInt();
-        System.out.println("b :");
-        int b = sc.nextInt();
-        System.out.println("c :");
-        int c = sc.nextInt();
-
-        if(a!=0){
-            float answer = (c-b)/a;
-            System.out.printf("Equation pass with x = %f\n",answer);
-        } else {
-            if(b==c){
-                System.out.println("The solution is all of x");
-            }else{
-                System.out.println("No Solution!");
-            }
-        }
+        System.out.println("Enter your name");
+        String name = sc.nextLine();
+        System.out.println("Hello : "+name);
     }
 }
