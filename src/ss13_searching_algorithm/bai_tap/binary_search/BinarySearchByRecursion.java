2d7bb044529f151301de8e419edbcd19581c6170
NguyenQuangHuu <nguyenquanghuu2010@gmail.com>
2022-03-04 13:24:47 +0700

    update bai tap

--- src/ss13_searching_algorithm/bai_tap/binary_search/BinarySearchByRecursion.java
+++ src/ss13_searching_algorithm/bai_tap/binary_search/BinarySearchByRecursion.java
@@ -4,39 +4,55 @@ import java.util.Arrays;
 
 public class BinarySearchByRecursion {
     public static void main(String[] args) {
-        int[] array = {5,3,7,1,9,7,10,12};
-        int result = binarySearchByRecursion(array,12);
+        int[] array = {1, 3, 5, 7, 9, 10, 12};
+        int length = array.length;
+        int result = binarySearchByRecursion(array, 0, length, 12);
         System.out.println(result);
     }
 
-    public static int binarySearchByRecursion(int[] array,int number){
-        for(int i = 0 ; i<array.length;i++){
-
-            for(int j = i+1;j<array.length-1;j++){
-                if(array[i]>array[j]){
-                    int temp = array[i];
-                    array[i]=array[j];
-                    array[j]=temp;
-                }
-            }
-
+    public static int binarySearchByRecursion(int[] array, int left, int right, int number) {
+//        for (int i = 0; i < array.length; i++) {
+//
+//            for (int j = i + 1; j < array.length - 1; j++) {
+//                if (array[i] > array[j]) {
+//                    int temp = array[i];
+//                    array[i] = array[j];
+//                    array[j] = temp;
+//                }
+//            }
+//
+//        }
+        if (right < left) {
+            return -1;
         }
 
-        System.out.println(Arrays.toString(array));
-        int left = 0;
-        int right = array.length;
+        int middle = (left + right )/ 2;
+        if (array[middle] == number) {
+            return middle;
+        }
+        if (number > array[middle]) {
+            return binarySearchByRecursion(array, middle + 1, right, number);
+        } else {
+            return binarySearchByRecursion(array, left, middle - 1, number);
+        }
 
-            while(left<right){
-                int middle = (left+right)/2;
-                if(array[middle]==number){
-                    return middle;
-                }else if(number>array[middle]){
-                    left = middle +1;
-                }else {
-                    right = middle -1;
-                }
-            }
-
-        return -1;
     }
+
+
+//        System.out.println(Arrays.toString(array));
+//        int left = 0;
+//        int right = array.length;
+//
+//            while(left<right){
+//                int middle = (left+right)/2;
+//                if(array[middle]==number){
+//                    return middle;
+//                }else if(number>array[middle]){
+//                    left = middle +1;
+//                }else {
+//                    right = middle -1;
+//                }
+//            }
+
 }
+
